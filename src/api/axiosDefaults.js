8278518286ae0eb374546f4092559dcfcd9e56b8
Rob Killick <robkillick@me.com>
2023-12-11 13:13:56 +0000

    add signup form and css styling

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://rkdev-drf-api-3511a67bfcda.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
