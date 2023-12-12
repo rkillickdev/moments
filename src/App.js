7a381a9a6ea202f95b025da9e5af09eee317f0a2
Rob Killick <robkillick@me.com>
2023-12-12 13:04:25 +0000

    implement functionality to determine logged in user

--- src/App.js
+++ src/App.js
@@ -5,20 +5,45 @@ import {Route,Switch} from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUSerContext = createContext();
 
 function App() {
+
+  const [currentUser, setCurrentUSer] = useState(null)
+
+  const handleMount = async () => {
+    try {
+      const {data} = await axios.get('dj-rest-auth/user/')
+      setCurrentUSer(data)
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    handleMount()
+  }, [])
+  
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/signin" render={() => <SignInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route render={() => <p>Page not found!</p>} />
-        </Switch>
-      </Container>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUSerContext.Provider calue={setCurrentUSer}>
+        <div className={styles.App}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={() => <h1>Home page</h1>} />
+              <Route exact path="/signin" render={() => <SignInForm />} />
+              <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route render={() => <p>Page not found!</p>} />
+            </Switch>
+          </Container>
+        </div>
+      </SetCurrentUSerContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
