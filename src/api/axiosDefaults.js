921cf0cf105f86f8247d3b313315d6a3b75ceb73
Rob Killick <robkillick@me.com>
2023-12-13 09:43:43 +0000

    add axios interceptors to automatically refresh access tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://rkdev-drf-api-3511a67bfcda.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
